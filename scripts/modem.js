45b763da1a596ffc9fbc711edc7b0368f2a4da5c
KristofferHojelse <34659757+KristofferHojelse@users.noreply.github.com>
2019-01-13 23:49:27 +0100

    mobile and border select

--- scripts/modem.js
+++ scripts/modem.js
@@ -8,3 +8,29 @@ document.getElementById("live").onclick = function(){
   $(offlinetext).toggleClass("offline-text");
   $(livebeacon).toggleClass("pulse-offline");
 }
+
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+  if (mediaquery.matches) {
+    $('.episode-title').each(function() {
+      var activeEpisode = $(this).attr('id');
+      if ($(this).isInViewport()) {
+        $('#episode-' + activeEpisode).addClass('episode-active');
+        console.log('#epsiode-' + activeEpisode);
+      } else {
+        $('#episode-' + activeEpisode).removeClass('episode-active');
+      }
+    });
+  }
+});
+
+var mediaquery = window.matchMedia("(max-width: 600px)");
