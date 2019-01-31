6b0f1eb99f0182922ef220aaab81df80914015b1
KristofferHojelse <34659757+KristofferHojelse@users.noreply.github.com>
2019-01-31 01:37:13 +0100

    minor changes

--- scripts/modem.js
+++ scripts/modem.js
@@ -91,7 +91,6 @@ xhttp.open("GET", "https://api.mixcloud.com/radiomodem/cloudcasts/", true);
 xhttp.send();
 
 //Mixcloud Widget
-/*
 var widget = Mixcloud.PlayerWidget(document.getElementById("my-widget-iframe"));
 widget.ready.then(function() {
   widget.play();
@@ -102,7 +101,6 @@ widget.ready.then(function() {
     widget.seek(91);
   };
 });
-*/
 
 //Mixcloud web scrape
 //document.querySelectorAll('div.show-description')[0].innerText
